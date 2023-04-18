22a589d9e45f37b5748c61390e8e1a4d68ea663c
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-18 17:29:20 +0300

    Add .png files support

--- react-setup-from-scratch/src/App.tsx
+++ react-setup-from-scratch/src/App.tsx
@@ -1,5 +1,12 @@
 import './styles.css'
+import image from './react.png'
 
 export const App = () => {
-    return (<h2>React setup from scratch, baby!</h2>);
+    return (
+        <>
+            <h2>React setup from scratch, baby!</h2>
+            <img src={image} alt="React logo" width="400" height="300" />
+        </>
+    );
+
 }
--- react-setup-from-scratch/webpack/webpack.config.js
+++ react-setup-from-scratch/webpack/webpack.config.js
@@ -20,8 +20,11 @@ module.exports = {
             {
                 test: /\.(css)$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/,
+                type: 'asset/resource'
             }
-
         ]
     },
     output: {
