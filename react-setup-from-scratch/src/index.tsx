523e6ef385f48fc8737eb4f79ba82e30d22eb22b
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-18 22:36:44 +0300

    Configure eslint

--- react-setup-from-scratch/src/index.tsx
+++ react-setup-from-scratch/src/index.tsx
@@ -1,6 +1,5 @@
-import ReactDOM from "react-dom"
 import { App } from "./App"
 import { createRoot } from "react-dom/client"
 
-const root = createRoot(document.getElementById("root") as any);
+const root = createRoot(document.getElementById("root") as Element);
 root.render(<App/>);
