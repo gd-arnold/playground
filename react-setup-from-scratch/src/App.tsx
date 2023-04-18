2c3968d676b1308b61db6d6b1e9619bf59cabe5f
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-18 22:07:18 +0300

    Implement hot reloading

--- react-setup-from-scratch/src/App.tsx
+++ react-setup-from-scratch/src/App.tsx
@@ -1,11 +1,15 @@
 import './styles.css'
 import image from './react.png'
+import { Counter } from './Counter';
 
 export const App = () => {
     return (
         <>
             <h2>React setup from scratch, baby!</h2>
             <img src={image} alt="React logo" width="400" height="300" />
+
+            {/* Counter component used for testing hot reloading */}
+            <Counter />
         </>
     );
 
--- react-setup-from-scratch/webpack/webpack.dev.js
+++ react-setup-from-scratch/webpack/webpack.dev.js
@@ -1,4 +1,12 @@
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
     mode: 'development',
-    devtool: 'cheap-module-source-map'
+    devServer: {
+        hot: true
+    },
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        new ReactRefreshPlugin()
+    ]
 }
