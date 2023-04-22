17fdd856d096c2df5b81fc97ca971b216317f6e4
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-22 20:10:58 +0300

    leetcode: two sum 2, optimized

--- leetcode/src/167-two-sum-ii-input-array-is-sorted.ts
+++ leetcode/src/167-two-sum-ii-input-array-is-sorted.ts
@@ -1,3 +1,27 @@
+// Context:
+// Array is sorted
+// Must use only constant extra space
+
+// Time: O(n)
+// Space: O(1)
+function twoSumOpt(numbers: number[], target: number): number[] {
+    let l = 0;
+    let r = numbers.length - 1;
+
+    while (l < r) {
+        const sum = numbers[l] + numbers[r];
+
+        if (sum === target) return [l + 1, r + 1];
+
+        if (sum > target) r--;
+        else l++;
+    }
+
+    return [l, r];
+}
+
+console.log(twoSumOpt([2,7,11,15], 9));
+
 // Time: O(n^2)
 // Space: O(1)
 function twoSum(numbers: number[], target: number): number[] {
