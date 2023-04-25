2adcf7ddbcd13487f33e7db5168e82bad2be1fc6
GeorgiArn <georgearnaudov1@gmail.com>
2023-04-26 02:56:03 +0300

    advanced-js: promisify fn

---
+++ advanced-js/promisify.js
@@ -0,0 +1,12 @@
+const promisify = fn => {
+    return function(...args) {
+        return new Promise((resolve, reject) => {
+            fn.call(this, ...args, (err, result) => {
+                if (err) reject(err);
+                else resolve(result);
+            })
+        });
+    }
+}
+
+module.exports = promisify;
