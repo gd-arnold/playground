39bd65af229ad3f71e05378d0036f9caa354936d
GeorgiArn <georgearnaudov1@gmail.com>
2023-05-03 13:24:47 +0300

    node dp: exercise 3.1

---
+++ js/node-dp/callbacks-and-events/exercises/3.1/index.js
@@ -0,0 +1,40 @@
+import { EventEmitter } from "events"
+import { readFile } from "fs"
+
+class FindRegex extends EventEmitter {
+    constructor(regex) {
+        super();
+        this.regex = regex;
+        this.files = [];
+    }
+
+    addFile(file) {
+        this.files.push(file);
+        return this;
+    }
+
+    find() {
+        process.nextTick(() => this.emit("findStart", this.files));
+
+        for (const file of this.files) {
+            readFile(file, "utf8", (err, data) => {
+                if (err) return this.emit("error", err);
+                this.emit("fileRead", file);
+
+                const match = data.match(this.regex);
+                if (match) match.forEach(elem => this.emit("found", elem, match));
+            })
+        }
+        return this;
+    }
+}
+
+const findRegexInstance = new FindRegex(/hello \w+/)
+findRegexInstance
+    .addFile("fileA")
+    .addFile("fileB.json")
+    .find()
+    .on("found", (file, match) => console.log(`Matched "${match}" in file
+        ${file}`))
+    .on("error", err => console.error(`Error emitted ${err.message}`))
+    .on("findStart", files => console.log(files));
