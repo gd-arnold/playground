09566ad55c59d941b5a69493eb939231e91830f7
GeorgiArn <georgearnaudov1@gmail.com>
2023-05-07 11:58:46 +0300

    k&r: exercise 1.09

---
+++ c/k&r/chapter_1/exercise_1_09/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int c, prev;
+
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' || prev != ' ') putchar(c);
+        prev = c;
+    }
+
+    return 0;
+}
