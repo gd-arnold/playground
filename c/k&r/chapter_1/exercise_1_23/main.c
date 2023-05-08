d439357d8b03343a259bf3d72a8020c5f1530a0d
GeorgiArn <georgearnaudov1@gmail.com>
2023-05-08 20:14:41 +0300

    k&r: exercise 1.23 (implicitly check values)

--- c/k&r/chapter_1/exercise_1_23/main.c
+++ c/k&r/chapter_1/exercise_1_23/main.c
@@ -36,26 +36,23 @@ void removeComments(char from[], char to[]) {
     for (int i = 0; c != '\0'; i++) {
         c = from[i];
 
-        if (c == '/' && inQuotes == 0 && inBlockComment == 0 && inLineComment == 0) {
-            if (from[i + 1] == '/') {
+        if (c == '/' && !inQuotes && !inBlockComment && !inLineComment) {
+            if (from[i + 1] == '/')
                 inLineComment = 1;
-            } else if (from[i + 1] == '*' && from[i + 2] == '*') {
+            else if (from[i + 1] == '*' && from[i + 2] == '*')
                 inBlockComment = 1;
-            }
         } else if (c == '"') {
-            if (inQuotes == 0) inQuotes = 1;
-            else inQuotes = 0;
-        } else if (c == '\n' && inLineComment == 1) {
+            if (inQuotes) inQuotes = 0;
+            else inQuotes = 1;
+        } else if (c == '\n' && inLineComment)
             inLineComment = 0;
-        }
 
-        if (inLineComment == 0 && inBlockComment == 0) {
+        if (!inLineComment && !inBlockComment) {
             to[j] = c;
             j++;
         }
 
-        if (c == '/' && inQuotes == 0 && inLineComment == 0 && from[i - 1] == '*' && from[i - 2] == '*') {
+        if (c == '/' && !inQuotes && !inLineComment && from[i - 1] == '*' && from[i - 2] == '*')
             inBlockComment = 0;
-        }
     }
 }
