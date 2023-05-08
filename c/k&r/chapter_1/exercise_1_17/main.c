e3580b84cbdad48146956ff53bb8679561a9ec57
GeorgiArn <georgearnaudov1@gmail.com>
2023-05-08 13:36:27 +0300

    k&r: exercise 1.17 (print lines after input)

--- c/k&r/chapter_1/exercise_1_17/main.c
+++ c/k&r/chapter_1/exercise_1_17/main.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 
-#define MINLINE 80
+#define MINLENGTH 81
+#define MAXLENGTH 1000
+#define MAXLINES 1000
+
+int getLine(char line[], int lim);
+void copy(char from[], char to[]);
 
 int main() {
-    char line[MINLINE];
-    int i = 1;
-    int c;
+    char lines[MAXLINES][MAXLENGTH];
+    char line[MAXLENGTH];
+    int i = 0, len = 0;
 
-    while (i > 0) {
-        for (i = 0; (c = getchar()) != EOF; i++) {
-            if (i < MINLINE) line[i] = c;
-            else if (i == MINLINE) {
-                printf("%s", line);
-                putchar(c);
-            }
-            else putchar(c);
-
-            if (c == '\n') {
-                i++;
-                break;
-            }
+    while ((len = getLine(line, MAXLENGTH)) > 0) {
+        if (len >= MINLENGTH) {
+            copy(line, lines[i]);
+            i++;
         }
     }
 
+    for (int j = 0; j < i; j++)
+        printf("%s", lines[j]);
+
     return 0;
+}
+
+int getLine(char line[], int lim) {
+    int i, c;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        line[i] = c;
+
+    if (c == '\n') {
+        line[i] = c;
+        i++;
+    }
+
+    line[i] = '\0';
+    return i;
+}
+
+void copy(char from[], char to[]) {
+    int i = 0;
+    while((to[i] = from[i]) != '\0') i++;
 }
