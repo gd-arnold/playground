ef4af6f1a488ede96d88695ef4d3dd2df20496dc
GeorgiArn <georgearnaudov1@gmail.com>
2023-06-14 22:35:23 +0300

    c: byte ordering

---
+++ c/byte-ordering/byte_ordering.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef unsigned char* byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%.2x ", *(start + i));
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void* x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main()
+{
+    int ival = 3;
+    float fval = (float) ival;
+    int* pval = &ival;
+
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+
+    return 0;
+}
