3b67c89167647c7f8019e3d722c08a2f0c2a218a
GeorgiArn <georgearnaudov1@gmail.com>
2023-12-04 21:41:21 +0200

    aoc2023 (c): day 3 (refactor)

--- advent-of-code/2023/c/day03/1.c
+++ advent-of-code/2023/c/day03/1.c
@@ -3,71 +3,95 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define MAXLEN 1024
 
-int main(int argc, char *argv[])
-{
-    FILE *input = fopen("input", "r");
+static unsigned count_rows(FILE* input, char* buffer);
+static unsigned count_cols(FILE* input, char* buffer);
 
-    // Calculate rows and cols
-    unsigned rows = 0;
-    unsigned cols = 0;
+static void fill_matrix(unsigned rows, char matrix[][rows], FILE* input, char* buffer);
+
+static unsigned calc_sum(unsigned rows, unsigned cols, char matrix[cols][rows]);
+
+int main(int argc, char* argv[]) {
+    FILE* input = fopen("input", "r");
 
     char buffer[MAXLEN];
+
+    // Fill matrix
+    unsigned rows = count_rows(input, buffer);
+    unsigned cols = count_cols(input, buffer);
+
+    char matrix[rows][cols];
+    fill_matrix(rows, matrix, input, buffer);
+
+    // Calculate sum
+    unsigned sum = calc_sum(rows, cols, matrix);
+
+    printf("%d\n", sum);
+    
+    return 0;
+}
+
+static unsigned count_rows(FILE* input, char* buffer)
+{
+    unsigned rows = 0;
     while (fgets(buffer, MAXLEN, input))
         rows++;
 
-    cols = strchr(buffer, '\n') - buffer;
-
-    char matrix[rows][cols];
-
-    // Reset file pointer
     fseek(input, 0, SEEK_SET);
 
-    // Fill matrix
+    return rows;
+}
+
+static unsigned count_cols(FILE* input, char* buffer)
+{
+    fgets(buffer, MAXLEN, input);
+    fseek(input, 0, SEEK_SET);
+
+    return strchr(buffer, '\n') - buffer;
+}
+
+static void fill_matrix(unsigned rows, char matrix[][rows], FILE* input, char* buffer)
+{
     for (unsigned i = 0; fgets(buffer, MAXLEN, input); i++)
         for (unsigned j = 0; buffer[j] != '\n'; j++)
             matrix[i][j] = buffer[j];
+}
 
-    // Calculate sum
+static unsigned calc_sum(unsigned rows, unsigned cols, char matrix[cols][rows])
+{
     unsigned sum = 0;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             unsigned num = 0;
             
             while (isdigit(matrix[i][j]))
                 num = num * 10 + (matrix[i][j++] - '0');
 
-            int isValid = 0;
-            if (num > 0)
-            {
+            bool is_valid = false;
+            if (num > 0) {
                 unsigned digits = floor(log10(num) + 1);
 
-                for (int k = i - 1; k <= i + 1; k++)
-                {
+                for (int k = i - 1; k <= i + 1; k++) {
                     if (k < 0 || k >= rows)
                         continue;
 
-                    for (int p = j - (digits + 1); p <= j; p++)
-                    {
+                    for (int p = j - (digits + 1); p <= j; p++) {
                         if (p < 0 || p >= cols)
                             continue;
 
                         if (matrix[k][p] != '.' && !isdigit(matrix[k][p]))
-                            isValid = 1;
+                            is_valid = 1;
                     }
                 }
             }
 
-            if (isValid)
+            if (is_valid)
                 sum += num;
         }
     }
 
-    printf("%d\n", sum);
-    
-    return 0;
+    return sum;
 }
--- advent-of-code/2023/c/day03/2.c
+++ advent-of-code/2023/c/day03/2.c
@@ -6,39 +6,66 @@
 
 #define MAXLEN 1024
 
-int main(int argc, char *argv[])
-{
-    FILE *input = fopen("input", "r");
+static unsigned count_rows(FILE* input, char* buffer);
+static unsigned count_cols(FILE* input, char* buffer);
 
-    // Calculate rows and cols
-    unsigned rows = 0;
-    unsigned cols = 0;
+static void fill_matrix(unsigned rows, char matrix[][rows], FILE* input, char* buffer);
+
+static unsigned calc_sum(unsigned rows, unsigned cols, char matrix[cols][rows]);
+
+int main(int argc, char* argv[]) {
+    FILE* input = fopen("input", "r");
 
     char buffer[MAXLEN];
+
+    // Fill matrix
+    unsigned rows = count_rows(input, buffer);
+    unsigned cols = count_cols(input, buffer);
+
+    char matrix[rows][cols];
+    fill_matrix(rows, matrix, input, buffer);
+
+    // Calculate sum
+    unsigned sum = calc_sum(rows, cols, matrix);
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
+static unsigned count_rows(FILE* input, char* buffer)
+{
+    unsigned rows = 0;
     while (fgets(buffer, MAXLEN, input))
         rows++;
 
-    cols = strchr(buffer, '\n') - buffer;
-
-    char matrix[rows][cols];
-
-    // Reset file pointer
     fseek(input, 0, SEEK_SET);
 
-    // Fill matrix
+    return rows;
+}
+
+static unsigned count_cols(FILE* input, char* buffer)
+{
+    fgets(buffer, MAXLEN, input);
+    fseek(input, 0, SEEK_SET);
+
+    return strchr(buffer, '\n') - buffer;
+}
+
+static void fill_matrix(unsigned rows, char matrix[][rows], FILE* input, char* buffer)
+{
     for (unsigned i = 0; fgets(buffer, MAXLEN, input); i++)
         for (unsigned j = 0; buffer[j] != '\n'; j++)
             matrix[i][j] = buffer[j];
+}
 
-    // Calculate sum
+static unsigned calc_sum(unsigned rows, unsigned cols, char matrix[cols][rows])
+{
     unsigned sum = 0;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if (matrix[i][j] == '*')
-            {
-                unsigned numTouching = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] == '*') {
+                unsigned num_touching = 0;
                 unsigned ratio = 1;
 
                 for (int k = i - 1; k <= i + 1; k++) {
@@ -62,18 +89,16 @@ int main(int argc, char *argv[])
                                 num = num * 10 + (matrix[k][p++] - '0');
 
                             ratio *= num;
-                            numTouching++;
+                            num_touching++;
                         }
                     }
                 }
 
-                if (numTouching == 2)
+                if (num_touching == 2)
                     sum += ratio;
             }
         }
     }
 
-    printf("%d\n", sum);
-    
-    return 0;
+    return sum;
 }
