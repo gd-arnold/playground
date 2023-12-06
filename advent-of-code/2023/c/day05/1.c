f8a53864927162f01182a611b060c0b0fc7205ce
GeorgiArn <georgearnaudov1@gmail.com>
2023-12-06 15:01:28 +0200

    aoc2023 (c): day 5, part 1

---
+++ advent-of-code/2023/c/day05/1.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+
+#define MAXLEN 1024
+
+#define RANGES 7
+
+typedef struct Range {
+    unsigned long dest;
+    unsigned long source;
+    unsigned long length;
+} Range;
+
+static unsigned 
+count_seeds(FILE* input, char buffer[], unsigned size);
+
+static void 
+fill_ranges_count(unsigned ranges_count[], FILE* input, char buffer[], unsigned size);
+
+static void 
+fill_seeds(unsigned seeds_count, unsigned long seeds[seeds_count], FILE* input, char buffer[], unsigned size);
+
+static void
+fill_ranges(Range** ranges[], unsigned ranges_count[], FILE* input, char buffer[], unsigned size);
+
+static unsigned long 
+map(Range* r[], unsigned long num); 
+
+int 
+main(int argc, char* argv[])
+{
+    FILE* input = fopen("input", "r");
+    char buffer[MAXLEN];
+
+    unsigned seeds_count = count_seeds(input, buffer, MAXLEN);
+    unsigned ranges_count[RANGES] = { 0 };
+    fill_ranges_count(ranges_count, input, buffer, MAXLEN);
+
+    Range** ranges[RANGES];
+    unsigned long seeds[seeds_count];
+
+    rewind(input);
+
+    fill_seeds(seeds_count, seeds, input, buffer, MAXLEN);
+    fill_ranges(ranges, ranges_count, input, buffer, MAXLEN);
+
+    fclose(input);
+
+    unsigned long lowest = ULONG_MAX;
+    for (unsigned i = 0; i < seeds_count; i++) {
+        unsigned long num = seeds[i];
+
+        for (unsigned range = 0; range < sizeof(ranges) / sizeof(ranges[0]); range++)
+            num = map(ranges[range], num);
+
+        if (num < lowest) lowest = num;
+    }
+
+    printf("%ld\n", lowest);
+
+    return 0;
+}
+
+static unsigned 
+count_seeds(FILE* input, char buffer[], unsigned size)
+{
+    unsigned seeds = 0;
+
+    fgets(buffer, MAXLEN, input);
+    for (char* ptr = buffer; (ptr = strchr(ptr, ' ')) != NULL; seeds++)
+        ptr++;
+    
+    return seeds;
+}
+
+static void 
+fill_ranges_count(unsigned ranges_count[], FILE* input, char buffer[], unsigned size)
+{
+    for (unsigned i = -1; fgets(buffer, MAXLEN, input);) {
+        if (isdigit(buffer[0])) ranges_count[i]++;
+        else if (buffer[0] != '\n') i++;
+    }
+}
+
+static void 
+fill_seeds(unsigned seeds_count, unsigned long seeds[seeds_count], FILE* input, char buffer[], unsigned size)
+{
+    fgets(buffer, size, input);
+    strtok(buffer, " ");
+    for (unsigned i = 0; i < seeds_count; i++)
+        seeds[i] = atol(strtok(NULL, " "));
+
+    rewind(input);
+}
+
+static void
+fill_ranges(Range** ranges[], unsigned ranges_count[], FILE* input, char buffer[], unsigned size)
+{
+    fgets(buffer, size, input);
+
+    for (int range = -1, range_indx = 0; fgets(buffer, size, input);) {
+        if (isdigit(buffer[0])) {
+            Range* r = (Range*) malloc(sizeof(Range));
+
+            r->dest = atol(strtok(buffer, " "));
+            r->source = atol(strtok(NULL, " "));
+            r->length = atol(strtok(NULL, "\n"));
+
+            ranges[range][range_indx] = r;
+
+            range_indx++;
+
+        } else if (buffer[0] != '\n') {
+            if (range >= 0)
+                ranges[range][range_indx] = NULL;
+
+            range++;
+            range_indx = 0;
+
+            ranges[range] = (Range**) malloc(sizeof(Range) * ranges_count[range] + 1);
+        }
+    }
+
+    rewind(input);
+
+}
+
+static unsigned long
+map(Range* r[], unsigned long num) 
+{
+    for (unsigned i = 0; r[i] != NULL; i++) {
+        if (num >= r[i]->source && num < r[i]->source + r[i]->length)
+            return num - (r[i]->source - r[i]->dest);
+    }
+
+    return num;
+}
